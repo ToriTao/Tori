4f0f38fbc8e12e9efa041cb8f722e2269f57a2d0
wuhua <120653918@qq.com>
2016-11-11 09:21:20 +0800

    First

---
+++ FirstTest.c
@@ -0,0 +1,21 @@
+/* ************************************************************************
+ *       Filename:  FirstTest.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年11月11日 09时09分58秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello Everyone!\n");
+	printf("My Name is Tori\n");
+	return 0;
+}
